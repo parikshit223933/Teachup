53d08e7b43e5614b89887ede65db7548354a1b97
parikshit223933 <pk223933@gmail.com>
2020-10-03 22:57:53 +0530

    created navs

---
+++ assets/js/classroom-navbar.js
@@ -0,0 +1,17 @@
+// TO CLOSE THE NAVBAR
+$('#close-nav').on('click', function () {
+    $('#navbar').toggleClass('show-navbar');
+});
+// TO OPEN THE NAVBAR
+$('#open-nav').on('click', function () {
+	$('#navbar').toggleClass('show-navbar');
+});
+
+// TO CLOSE THE MESSAGES NAVBAR
+$('#close-messages').on('click', function () {
+    $('#navbar-messages').toggleClass('show-navbar');
+});
+// TO OPEN THE MESSAGES NAVBAR
+$('#open-messages').on('click', function () {
+	$('#navbar-messages').toggleClass('show-navbar');
+});
