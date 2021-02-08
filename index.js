a0dff6261f652f4953abca84f77d6ce81d2f3dc4
Parikshit Singh <pk223933@gmail.com>
2021-02-08 11:56:40 +0530

    changed port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT||8000;
 const routes = require('./routes');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
